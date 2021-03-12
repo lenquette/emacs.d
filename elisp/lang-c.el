8bbd98bc9e892826e2e8e2e1c3e25ede129bc985
Frédéric Lenquette <frederic@lenquette.com>
2021-03-12 11:06:41 +0100

    evolution config c/c++

--- elisp/lang-c.el
+++ elisp/lang-c.el
@@ -12,7 +12,7 @@
   ;; "python": What Python developers use for extension modules
   ;; "java": The default style for java-mode (see below)
   ;; "user": When you want to define your own style
-  (setq c-default-style "linux") ;; set style to "linux"
+  (setq c-default-style "stroustrup") ;; set style to "stroustrup"
   (setq gdb-many-windows t ;; use gdb-many-windows by default
 	gdb-show-main t))
 
@@ -55,8 +55,9 @@
 
 (use-package cc-mode
   :init
-  (define-key c-mode-map  [(tab)] 'company-complete)
-  (define-key c++-mode-map  [(tab)] 'company-complete))
+  ;;(define-key c-mode-map  [(tab)] 'company-complete)
+  ;;(define-key c++-mode-map  [(tab)] 'company-complete)
+  )
 
 ;; git@github.com:syohex/emacs-counsel-gtags.git
 ;(use-package counsel-gtags
